1433900c7b2baaf5df0fa7522ed97b3bcc81d36c
luckyEason <2321676272@qq.com>
2023-07-06 12:14:21 +0800

    Site updated: 2023-07-06 12:14:19

--- js/sitetime.js
+++ js/sitetime.js
@@ -12,7 +12,7 @@ function siteTime(){
     var todayHour = today.getHours();
     var todayMinute = today.getMinutes();
     var todaySecond = today.getSeconds();
-    var t1 = Date.UTC(2022,6,4,12,00,00);
+    var t1 = Date.UTC(2023,6,6,12,00,00);
     var t2 = Date.UTC(todayYear,todayMonth,todayDate,todayHour,todayMinute,todaySecond);
     var diff = t2-t1;
     var diffYears = Math.floor(diff/years);
